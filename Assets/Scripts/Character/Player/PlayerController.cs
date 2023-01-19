22ad486182fe80acd18b3b19b31bbb97c497d36e
rifkybujana <rifkybujanabisri@gmail.com>
2023-01-18 18:41:42 -0800

    basic player horizontal move

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(
+            xInput * speed,
+            rb.velocity.y
+        );
     }
 }
